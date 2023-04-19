0c7a9a91aacf57a9b6a7e901685520b1ab7341f4
Ilnaz Khasanov <ilnaz.khasanoff2003@gmail.com>
2023-04-19 13:17:23 +0300

    Added GroupsView

---
+++ CollegeStatictics/ViewModels/RichContentDialog.cs
@@ -0,0 +1,36 @@
+﻿using ModernWpf.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace CollegeStatictics.ViewModels
+{
+    public class RichContentDialog : ContentDialog
+    {
+        public new ICommand PrimaryButtonCommand
+        {
+            get => (ICommand)GetValue(PrimaryButtonCommandProperty);
+            set
+            {
+                SetValue(PrimaryButtonCommandProperty, value);
+
+                value.CanExecuteChanged += delegate { IsPrimaryButtonEnabled = value.CanExecute(PrimaryButtonCommandParameter); };
+            }
+        }
+
+        public new ICommand SecondaryButtonCommand
+        {
+            get => (ICommand)GetValue(SecondaryButtonCommandProperty);
+            set
+            {
+                SetValue(SecondaryButtonCommandProperty, value);
+
+                value.CanExecuteChanged += delegate { IsSecondaryButtonEnabled = value.CanExecute(SecondaryButtonCommandParameter); };
+            }
+        }
+    }
+}
